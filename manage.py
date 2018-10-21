feea4a5c19bdb94fac861fa08d2fb61a7d8f00ec
krishnanath <isabella0484@gmail.com>
2018-10-21 21:07:22 +0530

    wsgi

--- manage.py
+++ manage.py
@@ -13,3 +13,19 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+"""
+WSGI config for djangoproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproject.settings')
+
+application = get_wsgi_application()
