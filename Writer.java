e2713f5e8d7e205338728a701d91a71561d7e104
krishnanath <isabella0484@gmail.com>
2018-10-27 20:27:54 +0530

    ...

--- Writer.java
+++ Writer.java
@@ -19,3 +19,71 @@ public class Writer {
         }
     }
 }
+
+/// mysql
+
+import java.sql.*;
+
+public class JdbcTest {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        String dbUrl = "jdbc:mysql://localhost:3306/demo";
+        String user = "student";
+        String pass = "student";
+
+        try {
+            // 1. Get a connection to database
+            myConn = DriverManager.getConnection(dbUrl, user, pass);
+
+            System.out.println("Database connection successful!\n");
+
+            // 2. Create a statement
+            myStmt = myConn.createStatement();
+
+            System.out.println("BEFORE UPDATE");
+
+            // displayEmployee(myConn, "Krishnanath", "Maly");
+            //
+            // int rowsAffected = myStmt.executeUpdate(
+            // "update employees"+
+            // "set email='krishna@gmail.com' " + "where last_name='anj' and
+            // first_name='lost' ");
+
+            // Insert a new employee
+
+            System.out.println("Inserting a new employee to db \n");
+
+            int rowsAffected = myStmt
+                    .executeUpdate("insert into employees" + "(last_name, first_name, email, department, salary)"
+                            + "values" + "('Maly', 'Krishnanath', 'maly@gmail.com', 'CEO', 888888.88)");
+
+            // 3. Execute SQL query
+            myRs = myStmt.executeQuery("select * from employees");
+
+            // 4. Process the result set
+            while (myRs.next()) {
+                System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            if (myRs != null) {
+                myRs.close();
+            }
+
+            if (myStmt != null) {
+                myStmt.close();
+            }
+
+            if (myConn != null) {
+                myConn.close();
+            }
+        }
+    }
+
+}
